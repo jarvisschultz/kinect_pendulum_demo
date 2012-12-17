5f4323a329e1c66ad49fa70f4088cf8ae7dab031
Jarvis Schultz <jschultz@u.northwestern.edu>
2012-12-17 10:47:22 -0600

    Saved two different windows settings in src/set_windows.sh

--- src/set_windows.sh
+++ src/set_windows.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 sleep 2
-# rbg window:
-wmctrl -r /camera/rgb/image_color -e 0,1600,0,400,250
+
+###############
+# MOVIE SIZE: #
+###############
+# # rgb window:
+# wmctrl -r /camera/rgb/image_color -e 0,1600,0,400,250
+# # depth window:
+# wmctrl -r /camera/depth/image -e 0,2000,0,400,250
+# # move the demo window:
+# wmctrl -r skeleton_interface.py -e 0,1600,275,800,300
+
+
+#########################
+# EXTERNAL MONITOR SIZE #
+#########################
+# rgb window:
+wmctrl -r /camera/rgb/image_color -e 0,1600,0,800,600
 # depth window:
-wmctrl -r /camera/depth/image -e 0,2000,0,400,250
+wmctrl -r /camera/depth/image -e 0,2400,0,800,600
 # move the demo window:
-# wmctrl -r pendulum_simulator.py -e 0,1600,600,1600,600
-wmctrl -r skeleton_interface.py -e 0,1600,275,800,300
+wmctrl -r skeleton_interface.py -e 0,1600,600,1600,600
+
