cf8964b360ad3ad553359c29c0e7f92b09b15d57
Jarvis Schultz <jschultz@u.northwestern.edu>
2013-01-24 18:14:10 -0600

    Created new files for the testing.  Slightly modified the mass of the pendulum in src/pendulum_simulator.py.  Modified src/set_windows.sh to be more intelligent about waiting for windows to exist.

---
+++ src/data_collection_interface.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+################
+# ROS IMPORTS: #
+################
+import roslib; roslib.load_manifest('kinect_pendulum_demo')
+import rospy
+import tf
+from skeletonmsgs_nu.msg import Skeletons
+from skeletonmsgs_nu.msg import Skeleton
+from skeletonmsgs_nu.msg import SkeletonJoint
+
+
+####################
+# NON ROS IMPORTS: #
+####################
+import sys, signal
+import os
+import math
+import copy
+from math import fmod, pi, copysign
+import testing_pendulum_simulator as ps
+import numpy as np
+import argparse
+
+####################
+# GLOBAL VARIABLES #
+####################
+SCALING = 5.0 ## real-world motion gets multiplied by this before being sent as
+              ## a reference
+
+
+class QtROS(ps.QThread):
+    def __init__(self, node_name):
+        super(QtROS, self).__init__()
+        rospy.init_node(node_name, log_level=rospy.INFO)
+        rospy.loginfo("QtROS connected to roscore")
+        self.quitfromgui = False
+
+    def quitnow(self):
+        self.quitfromgui = True
+
+    def run(self):
+        while not rospy.is_shutdown() and not self.quitfromgui:
+            rospy.sleep(1/30.0)
+        if self.quitfromgui:
+            rospy.loginfo("emitting rosQuits() signal")
+            self.emit(ps.SIGNAL("rosQuits()"))
+
+
+
+class PendulumController:
+    def __init__(self, DW, args):
+        rospy.loginfo("Starting pendulum controller...")
+
+        # define a subscriber for the skeleton information:
+        self.skel_sub = rospy.Subscriber("skeletons", Skeletons,
+                                         self.skelcb)
+        self.DW = DW
+        self.first_flag = True
+        self.already_reset_flag = True
+        self.base_pos = 0.0
+        # filter parameters:
+        self.falpha = 0.25
+        self.gamma = 0.05
+        self.a_low = 0.01
+        self.a_high = 0.50
+        self.v_high = 0.008
+        self.v_low = 0.001
+        self.bn = 0.0
+        self.prebn = 0.0
+        self.prex = 0.0
+        # testing parameters:
+        
+
+
+    def skelcb(self, data):
+        # get RH position, and set its value
+        if len(data.skeletons) == 0:
+            return
+        skel = data.skeletons[0]
+
+        pos = skel.right_hand.transform.translation.x
+
+        # run filter:
+        if not self.first_flag:
+            vn = np.abs(pos - self.prex)
+            if vn < self.v_low:
+                self.falpha = self.a_low
+            elif self.v_low <= vn <= self.v_high:
+                self.falpha = self.a_high + ((vn-self.v_high)/(self.v_low-self.v_high))*\
+                  (self.a_low-self.a_high)
+            elif vn > self.v_high:
+                self.falpha = self.a_high
+            else:
+                self.falpha = (self.a_high+self.a_low)/2.0
+            pos = self.falpha*pos + (1-self.falpha)*(self.prex+self.bn)
+            self.bn = self.gamma*(pos-self.prex) + (1-self.gamma)*self.prebn
+            self.prebn = self.bn
+            self.prex = pos
+
+        # If the left hand is above the hip, then we are active:
+        hand_height = skel.left_hand.transform.translation.y
+        hip_height = skel.left_hip.transform.translation.y
+        if hand_height < hip_height:
+            self.already_reset_flag = False
+            if self.first_flag: # find offset and reset filter:
+                self.base_pos = pos
+                self.first_flag = False
+                self.bn = 0.0
+                self.prex = pos
+                self.prebn = 0.0
+            pos -= self.base_pos
+            self.DW.mouse_pos = np.array([(SCALING)*pos])
+            # /float(self.DW.num_links)
+        else:
+            self.first_flag = True
+
+        hand_height_r = skel.right_hand.transform.translation.y
+        hip_height_r = skel.right_hip.transform.translation.y
+        if hand_height > hip_height and hand_height_r > hip_height_r:
+            if not self.already_reset_flag:
+                rospy.loginfo("Reset!")
+                self.DW.reset_flag = True
+                self.already_reset_flag = True
+
+        return
+
+
+def main():
+    # define all arguments that can be passed in:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--name", required=True,
+                        help="the test subject's name or identification")
+    parser.add_argument("-l", "--learning", type=bool, default=False,
+                        help="Set to true means that we will modify the "\
+                        "trust as the test progresses")
+    parser.add_argument("-g", "--lefty", type=bool, default=False,
+                        help="set true if subject is left-handed")
+    parser.add_argument("-i", "--index", type=int, default=30,
+                        help="set the index to start the test on")
+    # parser.add_argument("-t", "--total", type=int, default=30,
+    #                     help="total number of trials to run")
+    args = parser.parse_args()
+
+
+    app = ps.QApplication(sys.argv)
+    demo = ps.DemoWindow()
+    demo.resize(*ps.DEFAULT_WINDOW_SIZE)
+    demo.show()
+
+    # start QtROS:
+    qtros = QtROS("skeleton_interface")
+    # connect quit signals:
+    ps.QObject.connect(qtros, ps.SIGNAL("rosQuits()"), app.quit)
+    ps.QObject.connect(app, ps.SIGNAL("aboutToQuit()"), qtros.quitnow)
+    ps.QObject.connect(qtros, ps.SIGNAL("rosQuits()"), demo.close)
+    # create pendulum controller:
+    pend = PendulumController(demo, args)
+
+    qtros.start()
+    signal.signal(signal.SIGINT, signal.SIG_DFL) # allows demo to be killed with Ctrl+C
+    sys.exit(app.exec_())
+
+
+if __name__=='__main__':
+    main()
--- src/set_windows.sh
+++ src/set_windows.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-sleep 2
+
+win1=skeletontracker
+win2=/camera/depth/image
+win3=skeleton_interface.py
+for i in {1..5}
+do
+    echo "checking for window existence... "${i}
+    exist1=`wmctrl -l |grep -c $win1`
+    exist2=`wmctrl -l |grep -c $win2`
+    exist3=`wmctrl -l |grep -c $win3`
+    if  (( $exist1 == 1 )) && (( $exist2 == 1 )) && (( $exist3 == 1 )) 
+	then 
+	echo "window exists!"
+	break
+    else
+	sleep 1
+    fi
+done
 
 ###############
 # MOVIE SIZE: #
@@ -16,9 +33,11 @@ sleep 2
 # EXTERNAL MONITOR SIZE #
 #########################
 # rgb window:
-wmctrl -r /camera/rgb/image_color -e 0,1600,0,800,600
+# wmctrl -r /camera/rgb/image_color -e 0,1600,0,800,600
+wmctrl -r $win1 -e 0,1600,0,800,600
 # depth window:
-wmctrl -r /camera/depth/image -e 0,2400,0,800,600
+wmctrl -r $win2 -e 0,2400,0,800,600
 # move the demo window:
-wmctrl -r skeleton_interface.py -e 0,1600,600,1600,600
+wmctrl -r $win3 -e 0,1600,600,1600,600
 
+exit 0
