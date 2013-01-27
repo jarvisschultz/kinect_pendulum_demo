f94715a49cd239cec8d5ddc5750207caa6c4815d
Jarvis Schultz <jschultz@u.northwestern.edu>
2013-01-26 20:37:01 -0600

    Just finished looking over the data a bunch using analysis/calculate_and_plot_errors.py.  Also created analysis/data_animator.py to play back captured data.

--- analysis/calculate_and_plot_errors.py
+++ analysis/calculate_and_plot_errors.py
@@ -2,7 +2,7 @@
 import os
 import sys
 from math import sin, cos
-from math import pi as mpi
+from math import pi
 import numpy as np
 from matplotlib.pyplot import hold, plot
 import matplotlib.pyplot as mp
@@ -16,8 +16,10 @@ import angle_utils as au
 # global constants:
 base_dir = '/home/jarvis/ros/packages/kinect_pendulum_demo/data/'
 NUM_TRIALS = 30
-
-
+MAX_ANGLE = pi/6.0
+BASE_COST = 602.0
+TF = 6.0
+DT = 0.02
 
 def norm_func(dat):
     """
@@ -29,8 +31,13 @@ def norm_func(dat):
 
     # now calculate the total error between q and qd throughout time horizon
     err = 0
+    # for q in dat['q']:
+    #     if np.abs(q[0]) > MAX_ANGLE:
+    #         return -1.0
     for q in dat['q']:
         err += np.linalg.norm(q-qd)
+    if err > BASE_COST:
+        return -1.0
     return err
 
 
@@ -41,6 +48,7 @@ class User:
         self.name = name
         assert ( group == 'fixed' or group == 'training' )
         self.group = group
+        self.data = None
 
 
     def fill_trials(self):
@@ -60,12 +68,40 @@ class User:
                 print 'Could not locate file: \r\n',fname,'\r\n'
             self.data.append(dat)
 
+    def cleanup_long_datasets(self):
+        if self.data == None:
+            return
+        for i,dat in enumerate(self.data):
+            if len(dat['q']) > 301:
+                self.data[i]['q'] = dat['q'][-301:]
+                self.data[i]['t'] = dat['t'][-301:]
+                self.data[i]['u'] = dat['u'][-301:]
+
+
     def calc_last_few_errors(self):
         self.last_errs = []
         for i in range(NUM_TRIALS-5, NUM_TRIALS):
             dat = self.data[i]
             err = norm_func(dat)
-            self.last_errs.append(err)
+            if err > 0:
+                self.last_errs.append(err)
+
+    def calc_all_errors(self):
+        self.errs = []
+        self.errs_trials = []
+        for i in range(NUM_TRIALS):
+            err = norm_func(self.data[i])
+            if err > 0:
+                self.errs.append(err)
+                self.errs_trials.append(i)
+
+            q = self.data[i]['q']
+            if len(q) > 301:
+                print "Data too long:",len(q)
+                print "name = ",self.name
+                print "trial = ",i,"\r\n"
+                print "Final time = ",self.data[i]['t'][-1]
+
 
 
 
@@ -85,34 +121,55 @@ users = [
     ]
 for u in users:
     u.fill_trials()
+    u.cleanup_long_datasets()
     u.calc_last_few_errors()
+    u.calc_all_errors()
 
 
+
+###############################
+# ANALYZING THE LAST 5 TRIALS #
+###############################
 fixvals = []
 trainvals = []
 fdat = []
 tdat = []
 hold(True)
 for u in users:
+    if len(u.last_errs) == 0:
+        break
     if u.group == 'fixed':
-        xvals = np.array([1]*1)
-        fixvals.append(min(u.last_errs))
+        xvals = np.array([1]*len(u.last_errs))
+        for e in u.last_errs: fixvals.append(e)
+        # xvals = np.array([1]*1)
+        # fixvals.append(min(u.last_errs))
         fdat.append([u.name, min(u.last_errs), np.mean(u.last_errs), np.std(u.last_errs)])
     elif u.group == 'training':
-        xvals = np.array([2]*1)
-        trainvals.append(min(u.last_errs))
+        xvals = np.array([2]*len(u.last_errs))
+        for e in u.last_errs: trainvals.append(e)
+        # xvals = np.array([2]*1)
+        # trainvals.append(min(u.last_errs))
         tdat.append([u.name, min(u.last_errs), np.mean(u.last_errs), np.std(u.last_errs)])
-    mp.plot(xvals, min(u.last_errs), 'o')
+    mp.plot(xvals, u.last_errs, 'o', markersize=10, alpha=.7)
+    # mp.plot(xvals, min(u.last_errs), 'o', markersize=10, alpha=.7)
 
-mp.errorbar(1, np.mean(fixvals), yerr=np.std(fixvals),
-            marker='o', color='k', ecolor='r', markersize=12,
+
+mp.errorbar(1, np.mean(fixvals), yerr=np.std(np.ravel(fixvals)),
+            marker='x', color='k', ecolor='k', markersize=16, mfc='None', mew = 2.0,
             linewidth=2, linestyle='--')
-mp.errorbar(2, np.mean(trainvals), yerr=np.std(trainvals),
-            marker='o', color='k', ecolor='r', markersize=12,
+mp.errorbar(2, np.mean(trainvals), yerr=np.std(np.ravel(trainvals)),
+            marker='x', color='k', ecolor='k', markersize=16, mfc='None', mew = 2.0,
             linewidth=2, linestyle='--')
-hold(False)
+
+mp.grid(True)
 mp.xlim([0,3])
-# mp.ylim([0,3000])
+mp.xticks([1,2], ['Fixed Trust', 'Training Trust'])
+ax = mp.gca()
+for tick in ax.xaxis.get_major_ticks(): tick.label.set_fontsize(16)
+# mp.title('All trials that never pass $\pm\pi/6$')
+mp.title('All trials that improved on base cost')
+mp.ylabel('$L_2$ error between interactive and reference trajectories', fontsize=16)
+hold(False)
 
 fdat.sort(key=lambda tup: tup[1])
 tdat.sort(key=lambda tup: tup[1])
@@ -130,9 +187,35 @@ print "-----------------------------------------"
 for d in fdat:
     print '{0:<10s}|{1:^10.3f}|{2:^10.3f}|{3:^10.3f}'.format(d[0],d[1],d[2],d[3])
 
-
 mp.show()
-
 mp.close()
 mp.cla()
 mp.clf()
+
+
+# ############################
+# # ANALYZING TIME HISTORIES #
+# ############################
+# hold(True)
+# mp.subplot(2,1,1)
+# for u in users:
+#     if u.group == 'training':
+#         mp.plot(u.errs_trials, u.errs, '-o')
+# mp.title('Training data time history, all data')
+# # mp.xlim([25, 30])
+# mp.grid(True)
+
+# mp.subplot(2,1,2)
+# for u in users:
+#     if u.group == 'fixed':
+#         mp.plot(u.errs_trials, u.errs, '-o')
+# mp.title('Fixed data time history, all data')
+# # mp.xlim([25, 30])
+
+# mp.grid(True)
+# hold(False)
+
+# mp.show()
+# mp.close()
+# mp.cla()
+# mp.clf()
